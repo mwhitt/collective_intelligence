5c9ad81f1d64da83d88909b689ae4ff51cc06223
Matthew Whittaker <mwhitt89@gmail.com>
2010-01-02 19:24:04 -0800

    added distance score to search ranking

--- lib/ch04/search.rb
+++ lib/ch04/search.rb
@@ -6,12 +6,13 @@ module Search
     
     def find_pages(query)
       pages = WebPage.all('word_locations.word' => {'$all' => query.split})
-      scored_list(pages, query)
+      pp scored_list(pages, query)
     end
     
     def scored_list(pages, query)
       weights = [[1.0, frequency_scores(pages, query)],
-                 [1.5, location_scores(pages, query)]]
+                 [1.0, location_scores(pages, query)],
+                 [2.0, distance_scores(pages, query)]]
                  
       total_weights = weights.inject(Hash.new(0)) do |col, wa|
         weight = wa[0]
@@ -23,26 +24,22 @@ module Search
       pages.inject([]){|col, page| col << [total_weights[page.id], page.url]}.sort.reverse.take(30)
     end
     
-    def normalize_scores(scores, use_small = false)
-      vsmall = 0.00001
-      if use_small
-        min_score = scores.values.min
-        scores.inject({}) do |col, kv|
-          key = kv[0]
-          score = kv[1]
-          col[key] = min_score / [vsmall, score].max
-          col
-        end
-      else
-        max_score = scores.values.max
-        max_score = vsmall if max_score == 0
-        scores.inject({}) do |col, kv|
-          key = kv[0]
-          score = kv[1]
-          col[key] = score / max_score
-          col
-        end
+    def distance_scores(pages, query)
+      terms = query.split
+      
+      if terms.size < 2
+        return pages.inject({}){|col, page| col[page.id] = 1.0; col}
       end
+      
+      distances = pages.inject({}){|col, page| col[page.id] = 1000000.0; col}
+      pages.each do |page|
+        word_locations = page.word_locations.collect{|wl| wl if terms.include?(wl.word)}.compact
+        dist = (1..word_locations.size - 1).inject(0) do |sum, i|
+          sum += (word_locations[i].location - word_locations[i - 1].location).abs
+        end.to_f
+        distances[page.id] = dist if dist < distances[page.id]
+      end
+      normalize_scores(distances, true)  
     end
     
     def location_scores(pages, query)
@@ -66,6 +63,28 @@ module Search
         col
       end
       normalize_scores(counts)
+    end
+    
+    def normalize_scores(scores, use_small = false)
+      vsmall = 0.00001
+      if use_small
+        min_score = scores.values.min
+        scores.inject({}) do |col, kv|
+          key = kv[0]
+          score = kv[1]
+          col[key] = min_score / [vsmall, score].max
+          col
+        end
+      else
+        max_score = scores.values.max
+        max_score = vsmall if max_score == 0
+        scores.inject({}) do |col, kv|
+          key = kv[0]
+          score = kv[1]
+          col[key] = score / max_score
+          col
+        end
+      end
     end
     
   end
